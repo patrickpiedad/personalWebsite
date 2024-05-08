d1b82435381613a8741cdaac120dc429a452ded1
patrickpiedad <patrickpiedad@gmail.com>
2024-05-08 08:10:04 +0200

    Added admin file to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('./finalDesign/assets')
     eleventyConfig.addPassthroughCopy('./finalDesign/images')
+    eleventyConfig.addPassthroughCopy('./finalDesign/admin')
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
     })
