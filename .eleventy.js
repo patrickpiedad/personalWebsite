e782a3cff8b583757fb01594669aa7b59507c756
patrickpiedad <patrickpiedad@gmail.com>
2024-11-12 22:11:29 +0100

    Revamp website and personal theme descriptions

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,17 @@
-const {DateTime} = require("luxon")
+const { DateTime } = require('luxon')
 
-module.exports = function(eleventyConfig){
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('./finalDesign/assets')
+	eleventyConfig.addPassthroughCopy('./finalDesign/images')
+	eleventyConfig.addPassthroughCopy('./finalDesign/admin')
+	eleventyConfig.addFilter('postDate', dateObj => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+	})
 
-    eleventyConfig.addPassthroughCopy('./finalDesign/assets')
-    eleventyConfig.addPassthroughCopy('./finalDesign/images')
-    eleventyConfig.addPassthroughCopy('./finalDesign/admin')
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-    })
-
-    return {
-        dir: {
-            input: "finalDesign",
-            output: "public"
-        }
-    }
+	return {
+		dir: {
+			input: 'finalDesign',
+			output: 'public',
+		},
+	}
 }
